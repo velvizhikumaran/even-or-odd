919b8bd3fd72e52f3a4ae090e8f5512ade3fa33c
velvizhikumaran <velvizhieee@gmail.com>
2016-07-14 11:43:59 +0530

    Rename EvenorOdd.java to Even.java

---
+++ Even.java
@@ -0,0 +1,22 @@
+# even-or-odd
+import java.util.*;
+
+
+public class Even {
+		public static void main(String[] args){
+			int b;
+		 Scanner a=new Scanner(System.in);
+		 b=a.nextInt();
+		 int i=2;
+		 if(b%2==0)
+		 {
+		 System.out.println("the given number is even");
+		 }
+		 else
+		 {
+		 System.out.println("the given nmber is odd");
+		 }
+		}
+}
+
+
